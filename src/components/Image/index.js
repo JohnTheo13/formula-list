fbe53dec6ef534edc9b7b4622043b3eaa98c619e
JohnTheo13 <theodosioujohn88@gmail.com>
2018-10-09 20:19:48 +0200

    fix slider

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -7,32 +7,27 @@ const animation =  keyframes`
   50% {margin-left: -100%;}
   60% {margin-left: -200%;}
   70% {margin-left: -200%;}
-  80% {margin-left: 0px;}
-  90% {margin-left: 0px;}
+  80% {margin-left: -300%;}
+  90% {margin-left: -300%;}
+  100% {margin-left: 0px}
 `
 
 const SlideContainer = styled.div`
-  overflow: hidden;
-  height: 40%;
-  width: 100%;
-  margin: 0 auto;
+  background-color: #000000;
   & > div {
-    width: calc(100% + 200%);
+    overflow: hidden;
     height: 40%;
-    animation: ${animation} 18s ease infinite;
-    & > img {
-      float: left;
-      height: 100%;
-      width: 33.33%;
-      ${'' /* &:first-child {
-        background: url('/images/first.jpg');
+    width: 60%;
+    margin: 0 auto;
+    & > div {
+      width: 400%;
+      height: 40%;
+      animation: ${animation} 18s ease infinite;
+      & > img {
+        float: left;
+        height: 100%;
+        width: 25%;
       }
-      &:nth-child(2) {
-        background: url('/images/second.jpg');
-      }
-      &:last-child {
-        background: url('/image/third.jpg');
-      } */}
     }
   }
 `
@@ -40,9 +35,12 @@ const SlideContainer = styled.div`
 const Image = () =>
   <SlideContainer>
     <div>
-      <img src="/images/first.jpg" alt="" />
-      <img src="/images/second.jpg" alt="" />
-      <img src="/images/third.jpg" alt="" />
+      <div>
+        <img src="/images/first.jpg" alt="" />
+        <img src="/images/second.jpg" alt="" />
+        <img src="/images/third.jpg" alt="" />
+        <img src="/images/fourth.jpg" alt="" />
+      </div>
     </div>
   </SlideContainer>
 export default Image
