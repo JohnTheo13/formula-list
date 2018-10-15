76c1497efe7fd05d53dc16b5b12b3b42d0409d8e
JohnTheo <ioannis.theodosiou@bluecarpet.eu>
2018-10-15 09:37:28 +0200

    more lint

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,25 +1,20 @@
-import styled, { css } from 'styled-components';
-import DropdownItem from './DropdownItem';
-import DropdownMenu from './DropdownMenu';
+import styled, { css } from 'styled-components'
+import DropdownItem from './DropdownItem'
+import DropdownMenu from './DropdownMenu'
 
 const Dropdown = styled.div`
-  position: relative;
-  height: 100%;
-  ${props =>
-    props.isOpen &&
-    css`
+  position: relative
+  height: 100%
+  ${props => props.isOpen
+    && css`
       ${DropdownMenu} {
-        display: block;
-        ${props.align === 'right' ? 'right: 0;' : 'left: 0;'};
+        display: block
+        ${props.align === 'right' ? 'right: 0' : 'left: 0'}
       }
-    `};
-`;
+    `}
+`
 
-Dropdown.Item = DropdownItem;
-Dropdown.Menu = DropdownMenu;
+Dropdown.Item = DropdownItem
+Dropdown.Menu = DropdownMenu
 
-Dropdown.defaultProps = {
-  align: 'left'
-};
-
-export default Dropdown;
+export default Dropdown
--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,4 +1,4 @@
-export { default as DropdownItem } from './DropdownItem';
-export { default as DropdownMenu } from './DropdownMenu';
+export { default as DropdownItem } from './DropdownItem'
+export { default as DropdownMenu } from './DropdownMenu'
 
-export { default } from './Dropdown';
+export { default } from './Dropdown'
